0812b53f1553835afd090b7e01879fc8bd0af7f9
Daniel Kapper <dkapper01@gmail.com>
2018-06-12 20:53:09 -0400

    Routes working and sending json to post man

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User model
+const User = require("../../models/User");
 
 // @routes  GET api/users/test
 // @desc    Tests users routes
 // @access  Public
-
-router.get("/test", (req, res) =>
+router.post("/test", (req, res) =>
   res.json({
-    msg: "Users Works"
+    msg: "Users test"
   })
 );
+
+// @routes GET api/users/register
+// @desc Register users
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400);
+    }
+  });
+
+  const avatar = gravatar.url(req.body.email, {
+    s: "200",
+    r: "pg",
+    d: "d"
+  });
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    avatar,
+    password: req.body.password
+  });
+
+  newUser.save();
+
+  res.json(newUser);
+});
 
 module.exports = router;

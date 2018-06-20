148d0e91c9de180d69c5e7622e0f065794f18076
Daniel Kapper <dkapper01@gmail.com>
2018-06-19 20:32:25 -0400

    added handle route in profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,11 +39,27 @@ router.get(
 );
 
 // @route   GET api/profile
-// @desc    Get current users profile
-// @access  Private
+// @desc    Get profile handle
+// @access  Public
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors); 
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err)); 
+});
 
 
-// @route   GET api/profile
+// @route   GET api/profile/handle/:handle
 // @desc    Get current users profile
 // @access  Private
 

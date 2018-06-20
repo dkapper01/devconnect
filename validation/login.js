33b3fa1898edf7d4ff4e663e0ac4cdcf7b4570e8
Daniel Kapper <dkapper01@gmail.com>
2018-06-19 20:07:57 -0400

    Profile model and routes working

--- validation/login.js
+++ validation/login.js
@@ -8,7 +8,7 @@ module.exports = function validateLoginInput(data) {
   data.password = !isEmpty(data.password) ? data.password : '';
 
   if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
+    errors.email = 'Email is not invalid';
   }
 
   if (Validator.isEmpty(data.email)) {
